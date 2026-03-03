3cca74007d83cfb65076d308340a2e056f258b70
C-hill10 <osuconnor@live.com>
2026-03-03 17:02:26 -0600

    XML tree parser works, still needs to get timing of each note

--- TabToFinger.py
+++ TabToFinger.py
@@ -1,4 +1,25 @@
-import guitarpro as guitar
 import xml.etree.ElementTree as ET
-if __name__ = "__main__":
-    tree=Et.parse(xmlfile)
+import argparse
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description="reads music XML file (from musescore) and outputs a list of tuples")
+    parser.add_argument('file',type=str)
+    args=parser.parse_args()
+    notelist=[]
+    try:
+        file=open(args.file,encoding="utf8")
+    except FileNotFoundError:
+        print("Error: file does not exist")
+    else:
+        with file:
+            tree=ET.parse(args.file)
+            root=tree.getroot()
+            part=root[-1]
+            print(part[0][2])
+            print( f"the bpm is {part[0].find('direction').find('sound').get('tempo')}")
+            for measure in part:
+                for note in measure.findall('note'):
+                    for spec in note.iter("technical"):
+                        string=int(spec[0].text)
+                        fret=int(spec[1].text)
+                        notelist.append((string, fret))
+            print(notelist)
